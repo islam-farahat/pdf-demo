614e21bcb29c4c702c300f69cf8aa25bbbfabdcf
islam-farahat <islam.mostafa.f@gmail.com>
2023-03-24 16:23:53 +0200

    add dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
+import { Data } from './dto';
 
 @Controller('pdf')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  async getPdf(@Res() res: Response, @Body() data): Promise<void> {
+  async getPdf(@Res() res: Response, @Body() data: Data): Promise<void> {
     const pdfLink = await this.appService.getPdf(data.name);
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=menu.pdf');
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('PDF Demo')
+    .setDescription('The PDF Demo API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('PDF Demo')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
